1ad3e3ec2163420049470d3f0ef05f6f4025391a
LaggerIsME <abajsadykov2003@gmail.com>
2022-10-01 14:44:24 +0600

    A

---
+++ server.py
@@ -0,0 +1,11 @@
+#Импорт библиотек 
+import socket
+import threading
+
+def main():
+    #AF_INET = Использовании IPv4
+    #SOCK_STREAM = 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if __name__ == '__main__':
+    main()
