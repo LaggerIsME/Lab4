d0c65b60de821570bb5fe5afeb72908a8d74beb0
LaggerIsME <abajsadykov2003@gmail.com>
2022-10-01 17:03:50 +0600

    check

--- client.py
+++ client.py
@@ -1,9 +1,45 @@
 import socket
 import threading
 
+
 host = '127.0.0.1'
 port = 2022
+
+def listen_for_msg_from_server(client):
+
+    while True:
+        msg = client.recv(2048).decode('utf-8')
+        if msg == '':
+            print('Сообщение пустое')
+        else:
+            username = msg.split(": ")[0]
+            content = msg.split(": ")[1]
+            print(f"[{username}] {content}")
+
+def send_message_to_server(client):
+
+    while True:
+        msg = input()
+        if(msg == ''):
+            print('Оно не должно быть пустым')
+            exit(0)
+        else:
+            client.sendall(msg.encode())
+
+def comm_to_server(client):
+
+    username = input('Введите прозвище: ')
+    if username == '':
+        print('Оно не должно быть пустым')
+        exit(0)
+    else:
+        client.sendall(username.encode())
+
+    threading.Thread(target = listen_for_msg_from_server, args = (client, )).start()
+    send_message_to_server(client)
+
 def main():
+    
     #Сокет пользователя
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #Попытка подключения
@@ -12,5 +48,8 @@ def main():
         print('Вы подключены!')
     except:
         print(f'Не удалось подключиться к порту {port} и хосту {host}')
+
+    comm_to_server(client)
+
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -6,33 +6,51 @@ host = '127.0.0.1'
 port = 2022
 clients_limit = 3
 actual_clients = [] #Список пользователей
-#Отправка сообщений 
+
+#Ожидание сообщения
 def listen_for_msg(client_socket, sender_username):
+    
     while True:
-        msg = client_socket.recv(100).decode('utf-8')
+        msg = client_socket.recv(2048).decode('utf-8')
         if msg == '':
             print(f'Сообщение от {sender_username} пустое')
         else:
             chat_msg = sender_username + ": " + msg 
-def send_message_to_all(sender_username, msg):
-    pass
+            send_message_to_all(chat_msg)
+#Отправка сообщений одному пользователю
+def send_message_to_client(client_socket, msg):
+    
+    client_socket.sendall(msg.encode())
+
+#Отправка сообщений всем пользователям
+def send_message_to_all(msg):
+    
+    for user in actual_clients:
+        send_message_to_client(user[1], msg)
 
 #Обращение к клиенту
 def client_handler(client_socket):
+    
     #Сервер ждет прозвища пользователя с максимальной длиной 100
     while True:
-        username = client_socket(100).decode('utf-8')
-        if username == '':
+        username = client_socket.recv(2048).decode('utf-8')
+        if username == '':  
             print('Никнейм пуст')
         else:
             actual_clients.append((username, client_socket))
+            connect_msg = "SERVER: " + f"{username} зашел(-ла) в чат"
+            send_message_to_all(connect_msg)
+            break
 
+    threading.Thread(target=listen_for_msg, args = (client_socket, username, )).start()
 
 def main():
+    
     #socket = класс для обработки информации между процессами 
     #AF_INET = Использовании IPv4
     #SOCK_STREAM = Использование TCP-протокола, из-за его надежности
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
     try:
         #Подключиться серверу к данному порту, с таким названием хоста
         server.bind((host, port))
@@ -40,15 +58,19 @@ def main():
     except:
         print(f'Не удалось подключиться к порту {port} и хосту {host}')
         pass 
+   
     #Лимит возможных подключений
     server.listen(clients_limit)
     #Постоянно принимать подключения
+    
     while True:
         #Принимаем информацию о пользователе
         client_socket, client_connection_info = server.accept()
         client_host = client_connection_info[0]
         client_port = client_connection_info[1]
         print(f'Пользователь удачно подключился через порт {client_port} и хост {client_host} ')
-        threading.Thread(target =client_handler(client_socket), args = (client_socket, )).start()
+        
+        threading.Thread(target = client_handler, args = (client_socket, )).start()
+
 if __name__ == '__main__':
     main()
