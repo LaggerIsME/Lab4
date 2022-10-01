ff9b7b6f631e5651879bc17c9ceb642a0ef9585f
LaggerIsME <abajsadykov2003@gmail.com>
2022-10-01 15:01:17 +0600

    и

--- server.py
+++ server.py
@@ -2,10 +2,22 @@
 import socket
 import threading
 
+host = '127.0.0.1'
+port = 2022
+users_limit = 3
+
 def main():
+    #socket = класс для обработки информации между процессами 
     #AF_INET = Использовании IPv4
-    #SOCK_STREAM = 
+    #SOCK_STREAM = Использование TCP-протокола, из-за его надежности
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        #Подключиться серверу к данному порту, с таким названием хоста
+        server.bind((host, port))
+    except:
+        print(f'Не удалось подключиться к порту {port} и хосту {host}')
+        pass 
+    server.listen(users_limit)
 
 if __name__ == '__main__':
     main()
