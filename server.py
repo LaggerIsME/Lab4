d0e773d090d1783a2661a860f4b210a7368376f1
LaggerIsME <abajsadykov2003@gmail.com>
2022-10-01 16:03:26 +0600

    new functions

--- server.py
+++ server.py
@@ -5,6 +5,28 @@ import threading
 host = '127.0.0.1'
 port = 2022
 clients_limit = 3
+actual_clients = [] #Список пользователей
+#Отправка сообщений 
+def listen_for_msg(client_socket, sender_username):
+    while True:
+        msg = client_socket.recv(100).decode('utf-8')
+        if msg == '':
+            print(f'Сообщение от {sender_username} пустое')
+        else:
+            chat_msg = sender_username + ": " + msg 
+def send_message_to_all(sender_username, msg):
+    pass
+
+#Обращение к клиенту
+def client_handler(client_socket):
+    #Сервер ждет прозвища пользователя с максимальной длиной 100
+    while True:
+        username = client_socket(100).decode('utf-8')
+        if username == '':
+            print('Никнейм пуст')
+        else:
+            actual_clients.append((username, client_socket))
+
 
 def main():
     #socket = класс для обработки информации между процессами 
@@ -27,5 +49,6 @@ def main():
         client_host = client_connection_info[0]
         client_port = client_connection_info[1]
         print(f'Пользователь удачно подключился через порт {client_port} и хост {client_host} ')
+        threading.Thread(target =client_handler(client_socket), args = (client_socket, )).start()
 if __name__ == '__main__':
     main()
